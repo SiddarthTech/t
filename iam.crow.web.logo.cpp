ecf6bbdfc352253833c3b32913df43e15fa3f6ca
SiddarthTech <jayeshpalikar7@gmail.com>
2026-06-23 15:23:44 +0530

    Delete iam.crow.web.logo.cpp

--- iam.crow.web.logo.cpp
+++
@@ -1,5 +0,0 @@
-<img src="logo.png" alt="Crow Logo Upload Hote Hi Yahan Dikhega! 🦅" class="logo" onerror="this.style.display='none';">
-   Ab yahan `logo.png` ko mita kar apni file ka exact naam daal dein, jaise: **`1000070631.png`**
- Agar aapne aakhiri code copy kiya tha, toh usme ek `onerror` ka tag laga tha jo image na milne par use hide kar deta hai. Is line ko ekdum simple aise likh dein:
-   ```html
-   <img src="1000070631.png" alt="Crow Logo" class="logo">
